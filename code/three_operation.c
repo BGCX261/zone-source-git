7ed3e805f6637fadbfe9c7176d59f2e15f1b1abf
yj <yj@localhost.localdomain>
2012-10-17 13:06:02 +0800

    routine

---
+++ code/three_operation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+	printf("(3 - 1)? : 1  = %d\n", (3 - 1)? : 1);
+	printf("(1 - 3)? : 1  = %d\n", (1 - 3)? : 1);
+	printf("(1 - 1)? : 1  = %d\n", (1 - 1)? : 1);
+
+	return 1;
+}
